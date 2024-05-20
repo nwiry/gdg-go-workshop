deb26ca607564a234da5d2c62393e21f934e726c
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2024-05-20 15:19:06 -0300

    Create hello-world.go

---
+++ source/hello-world.go
@@ -0,0 +1,8 @@
+package main // Pacote principal do programa - Aqui é onde toda a mágica acontece
+
+import "fmt" // É a partir deste pacote que utilizamos a função para imprimir nosso texto na tela
+
+// main - Tudo que estiver aqui dentro será utilizado em nosso código durante a sua execução
+func main() {
+	fmt.Println("Hello World!") // Imprimimos na tela: "Olá Mundo!"
+}
